242372e239522f29acf2ed8db303c6f041ddf7be
MQInfer12 <maummq@gmail.com>
2022-10-22 19:23:43 -0400

    Devolver usuario al crear en admin y estado 209

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -75,7 +75,7 @@ class UserController extends Controller
 
         $user->save();
 
-        return response()->json(["mensaje" => "se guardo correctamente"], 201);
+        return response()->json(["mensaje" => "se guardo correctamente", "user" => $user], 209);
     }
 
     public function show($id)
