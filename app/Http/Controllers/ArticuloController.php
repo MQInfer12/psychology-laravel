f2d7d7170d0dccf7d0c387c2b9141bf396b29915
MQInfer12 <maummq@gmail.com>
2022-12-11 11:59:32 -0400

    Documento

--- app/Http/Controllers/ArticuloController.php
+++ app/Http/Controllers/ArticuloController.php
@@ -23,6 +23,7 @@ class ArticuloController extends Controller
         foreach(Storage::disk("public")->files() as $file) {
             $name = str_replace("public/","",$file);
             $downloadLink = route("download", $name);
+            dd($downloadLink);
             $files[] = [
                 "name" => $name,
                 "downloadLink" => $downloadLink
