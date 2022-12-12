acc3337a7c6043cdf8be0641e9e9963bcc783c7d
MQInfer12 <maummq@gmail.com>
2022-12-11 22:28:07 -0400

    Prueba final

--- app/Http/Controllers/ArticuloController.php
+++ app/Http/Controllers/ArticuloController.php
@@ -6,6 +6,7 @@ use App\Models\Articulo;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Storage;
+use Svg\Gradient\Stop;
 
 class ArticuloController extends Controller
 {
@@ -17,20 +18,25 @@ class ArticuloController extends Controller
     public function store(Request $request)
     {
         $file = $request->documento;
-        $file->storeAs('', $request->titulo.'.'.$file->extension(), 'public');
+        $name = $request->titulo.'.'.$file->extension();
+        $file->storeAs('', $name, 'public');
+        
+        $articulo = new Articulo();
+        $articulo->id_docente = $request->id_docente;
+        $articulo->titulo = $request->titulo;
+        $articulo->descripcion = $articulo->descripcion;
+        $articulo->documento = $name;
 
-        $files = [];
-
-        foreach(Storage::disk("public")->files() as $file) {
+        /* foreach(Storage::disk("public")->files() as $file) {
             $name = str_replace("public/","",$file);
             $asset = asset("storage/".$name);
             $files[] = [
                 "name" => $name,
                 "asset" => $asset
             ];
-        }
+        } */
 
-        return response()->json(["files" => $files]);
+        return response()->json(["message" => "Se guardo correctamente"], 201);
     }
 
     public function show($id)
@@ -38,12 +44,12 @@ class ArticuloController extends Controller
         //
     }
     
-    public function showArticlesByDocente($id_docente)
+    public function getArticlesByDocente($id_docente)
     {
         /* $articles = DB::select("SELECT * FROM articulos WHERE id_docente='$id_docente' ORDER BY id");
         return $articles; */
-        $files = Storage::disk("public")->files();
-        return $files[2];
+        $file = "storage/prueba2.pdf";
+        return response()->file($file);
     }
 
     public function update(Request $request, $id)
